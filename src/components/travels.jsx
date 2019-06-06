9adc3552aaf62822e26ae8a3203df06afa03c393
“Steven” <steven160894@gmail.com>
2019-06-06 18:35:41 +0200

    formulario que añade destinosa la api

--- src/components/travels.jsx
+++ src/components/travels.jsx
@@ -8,8 +8,12 @@ class Travels extends Component {
     constructor() {
         super();
         this.state = {
-            travels: []
+            travels: [],
+            destino: "",
+            imagen: "",
         }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     componentDidMount() {
@@ -20,10 +24,38 @@ class Travels extends Component {
                 this.setState({ travels });
             })
     }
+    handleChange(event) {
+        this.setState({ destino: event.target.value })
+    }
+    handleChangeImagen(event) {
+        this.setState({ imagen: event.target.value })
+    }
+    handleSubmit(event) {
+        console.log('A travel was suubmited: ' + this.state.destino);
+        
+        axios.post('http://localhost:3000/api/travel', {destino:  this.state.destino, imagen: this.state.imagen})
+            .then(res => {
+                console.log(res.data);
+                this.setState({
+               travels:[
+                   ...this.state.travels,
+                    res.data
+                ]
+                })
+            })
+        event.preventDefault();
+    }
 
     render() {
         return <div>
             {(this.state.travels).map(travel => <Travel {...travel} />)}
+            <form onSubmit={this.handleSubmit}>
+                Añadir Destino:
+                <input name="destino" value={this.state.destino} onChange={(e) => this.handleChange(e)} />
+                <input name="image" value={this.state.imagen} onChange={(e) => this.handleChangeImagen(e)} />
+                <input type="submit"/>
+
+            </form>
         </div>
     }
 }
