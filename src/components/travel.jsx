d760896cb87cf9a91371280ab475efba7811d7fd
“Steven” <steven160894@gmail.com>
2019-06-10 12:03:33 +0200

    completado de formulario

--- src/components/travel.jsx
+++ src/components/travel.jsx
@@ -4,6 +4,9 @@ function travel(props) {
     return <div id="travels">
         <h1>{props.destino}</h1>
         <img src={props.imagen} alt="" />
+        <h2>{props.precio}</h2>
+        <h3>{props.fecha_inicio}</h3>
+        <h3>{props.fecha_fin}</h3>
     </div>
 }
 
--- src/components/travels.jsx
+++ src/components/travels.jsx
@@ -11,9 +11,15 @@ class Travels extends Component {
             travels: [],
             destino: "",
             imagen: "",
+            precio: "",
+            fecha_inicio: "",
+            fecha_fin: ""
         }
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.handleChangePrecio = this.handleChangePrecio.bind(this);
+        this.handleChangeFechaInicio = this.handleChangeFechaInicio.bind(this);
+        this.handleChangeFechaFin = this.handleChangeFechaFin.bind(this);
     }
 
     componentDidMount() {
@@ -24,25 +30,45 @@ class Travels extends Component {
                 this.setState({ travels });
             })
     }
+
     handleChange(event) {
         this.setState({ destino: event.target.value })
     }
+
     handleChangeImagen(event) {
         this.setState({ imagen: event.target.value })
     }
+
+    handleChangePrecio(event) {
+        this.setState({ precio: event.target.value })
+    }
+
+    handleChangeFechaInicio(event) {
+        this.setState({ fecha_inicio: event.target.value })
+    }
+
+    handleChangeFechaFin(event) {
+        this.setState({ fecha_fin: event.target.value })
+    }
+
     handleSubmit(event) {
         console.log('A travel was suubmited: ' + this.state.destino);
-        
-        axios.post('http://localhost:3000/api/travel', {destino:  this.state.destino, imagen: this.state.imagen})
-            .then(res => {
-                console.log(res.data);
-                this.setState({
-               travels:[
-                   ...this.state.travels,
+
+        axios.post('http://localhost:3000/api/travel', {
+            destino: this.state.destino,
+            imagen: this.state.imagen,
+            precio: this.state.precio,
+            fecha_inicio: this.state.fecha_inicio,
+            fecha_fin: this.state.fecha_fin
+        }).then(res => {
+            console.log(res.data);
+            this.setState({
+                travels: [
+                    ...this.state.travels,
                     res.data
                 ]
-                })
             })
+        })
         event.preventDefault();
     }
 
@@ -50,10 +76,17 @@ class Travels extends Component {
         return <div>
             {(this.state.travels).map(travel => <Travel {...travel} />)}
             <form onSubmit={this.handleSubmit}>
-                Añadir Destino:
+                <label>Añadir Destino:</label>
                 <input name="destino" value={this.state.destino} onChange={(e) => this.handleChange(e)} />
+                <label>Añadir Imagen: </label>
                 <input name="image" value={this.state.imagen} onChange={(e) => this.handleChangeImagen(e)} />
-                <input type="submit"/>
+                <label>Añadir Precio:</label>
+                <input name="image" value={this.state.precio} onChange={(e) => this.handleChangePrecio(e)} />
+                <label>Añadir fecha de inicio:</label>
+                <input name="image" value={this.state.fecha_inicio} onChange={(e) => this.handleChangeFechaInicio(e)} />
+                <label>Añadir fecha de fin:</label>
+                <input name="image" value={this.state.fecha_fin} onChange={(e) => this.handleChangeFechaFin(e)} />
+                <input type="submit" />
 
             </form>
         </div>
